ba5d47ef207002de4c9c05f3986c6f492d4e8cc5
Ornchann Lyhov <user@Users-MacBook-Pro.local>
2025-10-31 11:12:53 +0700

    feat: Update greeting message

--- test/test.js
+++ test/test.js
@@ -3,9 +3,9 @@ const app = require('../src/app.js');
 const { expect } = require('chai');
 
 describe('GET /', () => {
-    it('should return Hello, CI/CD', async () => {
+    it('should return Hello, Pull Request', async () => {
         const res = await request(app).get('/');
         expect(res.status).to.equal(200);
-        expect(res.text).to.equal('Hello, CI/CD');
+        expect(res.text).to.equal('Hello, Pull Request');
     });
 });
